752589b467b11e209574119d3a64bc9f46357da8
Mentor <mentor@palokaj.co>
2015-09-23 13:31:38 +0200

    Remove popup on pages

--- functions/popper.php
+++ functions/popper.php
@@ -76,7 +76,7 @@ function wpsp_functionality() {
 	</script>
 	<?php
 }
-if ( ($wpsp_config['wpsp_active'] == "on") && !$_COOKIE['wpsp_silent'] ) {
+if ( ($wpsp_config['wpsp_active'] == "on") && !$_COOKIE['wpsp_silent'] && !is_page() ) {
 	add_action( 'wp_footer', 'wpsp_functionality' );
 }
 ?>
